53779ad91c10f77944f36a86b1847e234185832b
wenjing <zhaoshimin@come-future.com>
2024-05-15 10:08:47 +0800

    +实现泛型LRUCache

---
+++ algorithm-fun/src/main/java/com/algorithm/design/LRUCache.java
@@ -0,0 +1,49 @@
+package com.algorithm.design;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author wenjing.zsm
+ * @version 1.0
+ * @title:
+ * @description:
+ * @since 2024/5/15 9:55 上午
+ */
+public class LRUCache <E, U> {
+
+    LinkedHashMap<E, U> cache;
+    int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new LinkedHashMap<>();
+    }
+
+    public void put(E key, U value) {
+        if (cache.containsKey(key)) {
+            cache.put(key, value);
+            refresh(key);
+        }
+        if(cache.size() >= capacity) {
+            cache.remove(cache.keySet().iterator().next());
+            cache.put(key, value);
+        }
+        cache.put(key, value);
+    }
+
+    public U get(E key) {
+        if (!cache.containsKey(key)) {
+            return null;
+        }
+        refresh(key);
+        return cache.get(key);
+    }
+
+    private void refresh(E key) {
+        if (!cache.containsKey(key)) {
+            return;
+        }
+        U remove = cache.remove(key);
+        cache.put(key, remove);
+    }
+}
---
+++ algorithm-fun/src/test/java/LRUCacheTest.java
@@ -0,0 +1,26 @@
+import com.algorithm.design.LRUCache;
+import org.junit.Test;
+
+/**
+ * @author wenjing.zsm
+ * @version 1.0
+ * @title:
+ * @description:
+ * @since 2024/5/15 10:06 上午
+ */
+public class LRUCacheTest {
+
+    @Test
+    public void testLRUCache() {
+        LRUCache<String, String> lruCache = new LRUCache<>(3);
+        lruCache.put("1", "1");
+        lruCache.put("2", "2");
+        lruCache.put("3", "3");
+        System.out.println(lruCache.get("1"));
+        System.out.println(lruCache.get("2"));
+        System.out.println(lruCache.get("3"));
+        lruCache.put("4", "4");
+        System.out.println(lruCache.get("1"));
+        System.out.println(lruCache.get("4"));
+    }
+}
